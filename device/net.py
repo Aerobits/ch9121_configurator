28d6f73b3a034f3312dfafd3acd0bc7464d2b5d4
SzymonKostrubiec1 <szymon.kostrubiec@aerobits.pl>
2025-01-09 08:13:11 +0100

    Ruff format everything, action to check formatting

--- device/net.py
+++ device/net.py
@@ -1,9 +1,12 @@
 from netifaces import ifaddresses, AF_INET
 from sys import exit
 
+
 def get_broadcast_address(interface: str):
     try:
-        return ifaddresses(interface)[AF_INET][0]['broadcast']
+        return ifaddresses(interface)[AF_INET][0]["broadcast"]
     except (ValueError, KeyError):
-        print('Automatic broadcast lookup failed. Is the interface specified correctly?')
+        print(
+            "Automatic broadcast lookup failed. Is the interface specified correctly?"
+        )
         exit(1)
--- device/store_config.py
+++ device/store_config.py
@@ -1,5 +1,6 @@
 import yaml
 
+
 # workaround to enforce ordering of the .yaml file
 def create_temporary_dict(full_config, name: str):
     partial_config = dict()
@@ -7,17 +8,16 @@ def create_temporary_dict(full_config, name: str):
 
     return partial_config
 
+
 def config_save(config, filename):
-    with open(filename, 'w') as file:
-        yaml.dump(create_temporary_dict(config, 'HW config'), file)
-        yaml.dump(create_temporary_dict(config, 'Default port config'), file)
-        yaml.dump(create_temporary_dict(config, 'Auxiliary port config'), file)
+    with open(filename, "w") as file:
+        yaml.dump(create_temporary_dict(config, "HW config"), file)
+        yaml.dump(create_temporary_dict(config, "Default port config"), file)
+        yaml.dump(create_temporary_dict(config, "Auxiliary port config"), file)
+
 
 def config_load(filename):
-
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         config = yaml.load(f, yaml.loader.FullLoader)
 
     return config
-
-
